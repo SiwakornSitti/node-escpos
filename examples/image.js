e23a0b54b7b85e2d9e744fa05a9215a8352332aa
vattanan buranapugdee <vattanan.bu@gmail.com>
2017-11-08 12:33:40 +0700

    Update Example

--- examples/image.js
+++ examples/image.js
@@ -1,11 +1,17 @@
 'use strict';
 const escpos = require('../');
 
-const device  = new escpos.USB();
+// const device  = new escpos.USB();
+const device  = new escpos.Serial('COM2',{
+  baudRate: 38400,
+  stopBits: 1,
+  parity: "none",
+  dataBits: 8,
+})
+
 const printer = new escpos.Printer(device);
 
 escpos.Image.load(__dirname + '/image.png', function(image){
-
   device.open(function(){
     printer
     .align('ct')
